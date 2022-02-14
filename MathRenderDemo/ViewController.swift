51bf633fc5f12803505928fc306e4b1235e1ab5f
Michael Griebling <sam@c-inspirations.com>
2022-02-14 16:11:06 -0500

    Update with readme and example images.

--- MathRenderDemo/ViewController.swift
+++ MathRenderDemo/ViewController.swift
@@ -17,9 +17,7 @@ class FontPickerDelegate : NSObject, UIPickerViewDelegate, UIPickerViewDataSourc
         self.fontNames.count
     }
     
-    func numberOfComponents(in pickerView: UIPickerView) -> Int {
-        1
-    }
+    func numberOfComponents(in pickerView: UIPickerView) -> Int { 1 }
     
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
         self.fontNames[row]
@@ -46,21 +44,19 @@ class ColorPickerDelegate : NSObject, UIPickerViewDelegate, UIPickerViewDataSour
         self.colors.count
     }
     
-    func numberOfComponents(in pickerView: UIPickerView) -> Int {
-        1
-    }
+    func numberOfComponents(in pickerView: UIPickerView) -> Int { 1 }
     
     func pickerView(_ pickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusing view: UIView?) -> UIView {
-        let label = UILabel(frame: CGRect(x: 0, y: 0, width: 30, height: 30))
+        let label = UILabel(frame: CGRect(x: 0, y: 0, width: 100, height: 25))
         label.backgroundColor = self.colors[row]
         return label
     }
     
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
         let color = self.colors[row]
-        self.controller.colorField.backgroundColor = color
-        self.controller.changeColor(color)
-        self.controller.colorField.resignFirstResponder()
+        controller.changeColor(color)
+        controller.colorField.backgroundColor = color
+        controller.colorField.resignFirstResponder()
     }
 }
 
@@ -81,6 +77,8 @@ class ViewController: UIViewController, UITextFieldDelegate {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
+
+        
         // Set up the font picker
         self.pickerDelegate = FontPickerDelegate()
         self.pickerDelegate.controller = self
@@ -93,7 +91,7 @@ class ViewController: UIViewController, UITextFieldDelegate {
         
         // Set up the color picker
         self.colorPickerDelegate = ColorPickerDelegate()
-        self.pickerDelegate.controller = self
+        self.colorPickerDelegate.controller = self
         picker = UIPickerView()
         picker.delegate = self.colorPickerDelegate
         picker.dataSource = self.colorPickerDelegate
@@ -105,33 +103,73 @@ class ViewController: UIViewController, UITextFieldDelegate {
         let contentView = UIView()
         self.addFullSizeView(contentView, to: scrollView)
         self.setEqualWidths(contentView, view2: scrollView)
-        self.setHeight(4350, for: contentView)
+        self.setHeight(5100, for: contentView)
         
         // Demo formulae
         // Quadratic formula
-        demoLabels.append(createMathLabel("\\text{ваш вопрос: }x = \\frac{-b \\pm \\sqrt{b^2-4ac}}{2a}", height: 60))
+        demoLabels.append(createMathLabel("\\text{Quadratic roots: }x = \\frac{-b \\pm \\sqrt{b^2-4ac}}{2a}", height: 60))
         addLabelAsSubview(demoLabels[0], to: contentView)
-        demoLabels[0].fontSize = 15
+        demoLabels[0].fontSize = 25
         // First label so set the height from the top
         let view = demoLabels[0]
         let views = ["view": view]
         NSLayoutConstraint.activate(NSLayoutConstraint.constraints(withVisualFormat: "V:|-(10)-[view]", options: .alignAllCenterX, metrics: nil, views: views))
         
         // make some more examples
-        demoLabels.append(createMathLabel("\\color{#ff3399}{(a_1+a_2)^2}=a_1^2+2a_1a_2+a_2^2", height: 40))
-        demoLabels.append(createMathLabel("\\cos(\\theta + \\varphi) = \\cos(\\theta)\\cos(\\varphi) - \\sin(\\theta)\\sin(\\varphi)", height:40))
+        demoLabels.append(createMathLabel("\\color{#ff3399}{(a_1+a_2)^2}=a_1^2+2a_1a_2+a_2^2", height: 50))
+        demoLabels.append(createMathLabel("\\cos(\\theta + \\varphi) = \\cos(\\theta)\\cos(\\varphi) - \\sin(\\theta)\\sin(\\varphi)", height:50))
         demoLabels.append(createMathLabel("\\frac{1}{\\left(\\sqrt{\\phi \\sqrt{5}}-\\phi\\right) e^{\\frac25 \\pi}} " +
-                                          "= 1+\\frac{e^{-2\\pi}} {1 +\\frac{e^{-4\\pi}} {1+\\frac{e^{-6\\pi}} {1+\\frac{e^{-8\\pi}} {1+\\cdots} } } }", height:80))
-        demoLabels.append(createMathLabel("\\sigma = \\sqrt{\\frac{1}{N}\\sum_{i=1}^N (x_i - \\mu)^2}", height:60))
-        demoLabels.append(createMathLabel("\\neg(P\\land Q) \\iff (\\neg P)\\lor(\\neg Q)", height:40))
-        demoLabels.append(createMathLabel("\\log_b(x) = \\frac{\\log_a(x)}{\\log_a(b)}", height:40))
-        demoLabels.append(createMathLabel("\\lim_{x\\to\\infty}\\left(1 + \\frac{k}{x}\\right)^x = e^k", height:40))
-        demoLabels.append(createMathLabel("\\int_{-\\infty}^\\infty \\! e^{-x^2} dx = \\sqrt{\\pi}", height:40))
-        demoLabels.append(createMathLabel("\\frac 1 n \\sum_{i=1}^{n}x_i \\geq \\sqrt[n]{\\prod_{i=1}^{n}x_i}", height:60))
-        demoLabels.append(createMathLabel("f^{(n)}(z_0) = \\frac{n!}{2\\pi i}\\oint_\\gamma\\frac{f(z)}{(z-z_0)^{n+1}}\\,dz", height:40))
+                                          "= 1+\\frac{e^{-2\\pi}} {1 +\\frac{e^{-4\\pi}} {1+\\frac{e^{-6\\pi}} {1+\\frac{e^{-8\\pi}} {1+\\cdots} } } }", height:100))
+        demoLabels.append(createMathLabel("\\sigma = \\sqrt{\\frac{1}{N}\\sum_{i=1}^N (x_i - \\mu)^2}", height:100))
+        demoLabels.append(createMathLabel("\\neg(P\\land Q) \\iff (\\neg P)\\lor(\\neg Q)", height:50))
+        demoLabels.append(createMathLabel("\\log_b(x) = \\frac{\\log_a(x)}{\\log_a(b)}", height:70))
+        demoLabels.append(createMathLabel("\\lim_{x\\to\\infty}\\left(1 + \\frac{k}{x}\\right)^x = e^k", height:70))
+        demoLabels.append(createMathLabel("\\int_{-\\infty}^\\infty \\! e^{-x^2} dx = \\sqrt{\\pi}", height:70))
+        demoLabels.append(createMathLabel("\\frac 1 n \\sum_{i=1}^{n}x_i \\geq \\sqrt[n]{\\prod_{i=1}^{n}x_i}", height:100))
+        demoLabels.append(createMathLabel("f^{(n)}(z_0) = \\frac{n!}{2\\pi i}\\oint_\\gamma\\frac{f(z)}{(z-z_0)^{n+1}}\\,dz", height:70))
+        demoLabels.append(createMathLabel("i\\hbar\\frac{\\partial}{\\partial t}\\mathbf\\Psi(\\mathbf{x},t) = " +
+                               "-\\frac{\\hbar}{2m}\\nabla^2\\mathbf\\Psi(\\mathbf{x},t) + V(\\mathbf{x})\\mathbf\\Psi(\\mathbf{x},t)", height:60))
+        demoLabels.append(createMathLabel("\\left(\\sum_{k=1}^n a_k b_k \\right)^2 \\le \\left(\\sum_{k=1}^n a_k^2\\right)\\left(\\sum_{k=1}^n b_k^2\\right)", height:100))
+        demoLabels.append(createMathLabel("{n \\brace k} = \\frac{1}{k!}\\sum_{j=0}^k (-1)^{k-j}\\binom{k}{j}(k-j)^n", height:80))
+        demoLabels.append(createMathLabel("f(x) = \\int\\limits_{-\\infty}^\\infty\\!\\hat f(\\xi)\\,e^{2 \\pi i \\xi x}\\,\\mathrm{d}\\xi", height:100))
+        demoLabels.append(createMathLabel("\\begin{gather}" +
+                                          "\\dot{x} = \\sigma(y-x) \\\\" +
+                                          "\\dot{y} = \\rho x - y - xz \\\\" +
+                                          "\\dot{z} = -\\beta z + xy" +
+                                          "\\end{gather}", height:120))
+        demoLabels.append(createMathLabel("\\vec \\bf V_1 \\times \\vec \\bf V_2 =  \\begin{vmatrix}" +
+                               "\\hat \\imath &\\hat \\jmath &\\hat k \\\\" +
+                               "\\frac{\\partial X}{\\partial u} &  \\frac{\\partial Y}{\\partial u} & 0 \\\\" +
+                               "\\frac{\\partial X}{\\partial v} &  \\frac{\\partial Y}{\\partial v} & 0" +
+                               "\\end{vmatrix}", height:120))
+        demoLabels.append(createMathLabel("\\begin{eqalign}" +
+                               "\\nabla \\cdot \\vec{\\bf{E}} & = \\frac {\\rho} {\\varepsilon_0} \\\\" +
+                               "\\nabla \\cdot \\vec{\\bf{B}} & = 0 \\\\" +
+                               "\\nabla \\times \\vec{\\bf{E}} &= - \\frac{\\partial\\vec{\\bf{B}}}{\\partial t} \\\\" +
+                               "\\nabla \\times \\vec{\\bf{B}} & = \\mu_0\\vec{\\bf{J}} + \\mu_0\\varepsilon_0 \\frac{\\partial\\vec{\\bf{E}}}{\\partial t}" +
+                               "\\end{eqalign}", height:250))
+        demoLabels.append(createMathLabel("\\begin{pmatrix}" +
+                               "a & b\\\\ c & d" +
+                               "\\end{pmatrix}" +
+                               "\\begin{pmatrix}" +
+                               "\\alpha & \\beta \\\\ \\gamma & \\delta" +
+                               "\\end{pmatrix} = " +
+                               "\\begin{pmatrix}" +
+                               "a\\alpha + b\\gamma & a\\beta + b \\delta \\\\" +
+                               "c\\alpha + d\\gamma & c\\beta + d \\delta " +
+                               "\\end{pmatrix}", height:100))
+        demoLabels.append(createMathLabel("\\frak Q(\\lambda,\\hat{\\lambda}) = " +
+                               "-\\frac{1}{2} \\mathbb P(O \\mid \\lambda ) \\sum_s \\sum_m \\sum_t \\gamma_m^{(s)} (t) +\\\\ " +
+                               "\\quad \\left( \\log(2 \\pi ) + \\log \\left| \\cal C_m^{(s)} \\right| + " +
+                               "\\left( o_t - \\hat{\\mu}_m^{(s)} \\right) ^T \\cal C_m^{(s)-1} \\right) ", height:160))
+        demoLabels.append(createMathLabel("f(x) = \\begin{cases}" +
+                               "\\frac{e^x}{2} & x \\geq 0 \\\\" +
+                               "1 & x < 0" +
+                               "\\end{cases}", height:80))
+        demoLabels.append(createMathLabel("\\color{#ff3333}{c}\\color{#9933ff}{o}\\color{#ff0080}{l}+\\color{#99ff33}{\\frac{\\color{#ff99ff}{o}}" + "{\\color{#990099}{r}}}-\\color{#33ffff}{\\sqrt[\\color{#3399ff}{e}]{\\color{#3333ff}{d}}}", height:70))
         
         for i in 1..<demoLabels.count {
-            demoLabels[i].fontSize = 15
+            demoLabels[i].fontSize = 25
             addLabel(withIndex: i, inArray: demoLabels, toView: contentView)
         }
         
@@ -141,7 +179,7 @@ class ViewController: UIViewController, UITextFieldDelegate {
         labels.append(createMathLabel("3+2-5 = 0", height:40))
         labels[0].backgroundColor = UIColor(hue:0.15, saturation:0.2, brightness:1.0, alpha:1.0)
         addLabelAsSubview(labels[0], to:contentView)
-        setVerticalGap(30, between:lastDemoLabel, and:labels[0])
+        setVerticalGap(50, between:lastDemoLabel, and:labels[0])
 
         // Infix and prefix Operators
         labels.append(createMathLabel("12+-3 > +14", height:40))
@@ -154,10 +192,117 @@ class ViewController: UIViewController, UITextFieldDelegate {
         // Latex commands
         labels.append(createMathLabel("5\\times(-2 \\div 1) = -10", height:40))
         labels[3].backgroundColor = UIColor(hue:0.15, saturation:0.2, brightness:1.0, alpha:1.0)
-        labels[3].textAlignment = .right;
+        labels[3].textAlignment = .right
         labels[3].contentInsets = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 20);
 
         labels.append(createMathLabel("-h - (5xy+2) = z", height:40))
+        
+        // Text mode fraction
+        labels.append(createMathLabel("\\frac12x + \\frac{3\\div4}2y = 25", height:60))
+        self.labels[5].labelMode = .text
+
+        // Display mode fraction
+        labels.append(createMathLabel("\\frac{x+\\frac{12}{5}}{y}+\\frac1z = \\frac{xz+y+\\frac{12}{5}z}{yz}", height:60))
+        self.labels[6].backgroundColor = UIColor(hue:0.15, saturation:0.2, brightness:1.0, alpha:1.0)
+        self.labels[6].contentInsets = UIEdgeInsets(top: 0, left: 20, bottom: 0, right: 0);
+        
+        // fraction in fraction in text mode
+        labels.append(createMathLabel("\\frac{x+\\frac{12}{5}}{y}+\\frac1z = \\frac{xz+y+\\frac{12}{5}z}{yz}", height:60))
+        self.labels[7].backgroundColor = UIColor(hue:0.15, saturation:0.2, brightness:1.0, alpha:1.0)
+        self.labels[7].labelMode = .text
+        
+        // Exponents and subscripts
+        // Large font
+        labels.append(createMathLabel("\\frac{x^{2+3y}}{x^{2+4y}} = x^y \\times \\frac{z_1^{y+1}}{z_1^{y+1}}", height:90))
+        self.labels[8].fontSize = 30;
+        self.labels[8].textAlignment = .center
+
+        // Small font
+        labels.append(createMathLabel("\\frac{x^{2+3y}}{x^{2+4y}} = x^y \\times \\frac{z_1^{y+1}}{z_1^{y+1}}", height:30))
+        self.labels[9].fontSize = 10;
+        self.labels[9].textAlignment = .center
+
+        // Square root
+        labels.append(createMathLabel("5+\\sqrt{2}+3", height:40))
+
+        // Square root inside square roots and with fractions
+        labels.append(createMathLabel("\\sqrt{\\frac{\\sqrt{\\frac{1}{2}} + 3}{\\sqrt5^x}}+\\sqrt{3x}+x^{\\sqrt2}", height:90))
+
+        // General root
+        labels.append(createMathLabel("\\sqrt[3]{24} + 3\\sqrt{2}24", height:40))
+
+        // Fractions and formulae in root
+        labels.append(createMathLabel("\\sqrt[x+\\frac{3}{4}]{\\frac{2}{4}+1}", height:60))
+
+        // Non-symbol operators with no limits
+        labels.append(createMathLabel("\\sin^2(\\theta)=\\log_3^2(\\pi)", height:60))
+
+        // Non-symbol operators with limits
+        labels.append(createMathLabel("\\lim_{x\\to\\infty}\\frac{e^2}{1-x}=\\limsup_{\\sigma}5", height:60))
+
+        // Symbol operators with limits
+        labels.append(createMathLabel("\\sum_{n=1}^{\\infty}\\frac{1+n}{1-n}=\\bigcup_{A\\in\\Im}C\\cup B", height:60))
+
+        // Symbol operators with limits text style
+        labels.append(createMathLabel("\\sum_{n=1}^{\\infty}\\frac{1+n}{1-n}=\\bigcup_{A\\in\\Im}C\\cup B", height:60))
+        self.labels[17].labelMode = .text
+
+        // Non-symbol operators with limits text style
+        labels.append(createMathLabel("\\lim_{x\\to\\infty}\\frac{e^2}{1-x}=\\limsup_{\\sigma}5", height:60))
+        self.labels[18].labelMode = .text
+
+        // Symbol operators with no limits
+        labels.append(createMathLabel("\\int_{0}^{\\infty}e^x \\,dx=\\oint_0^{\\Delta}5\\Gamma", height:60))
+
+        // Test italic correction for large ops
+        labels.append(createMathLabel("\\int\\int\\int^{\\infty}\\int_0\\int^{\\infty}_0\\int", height:60))
+
+        // Test italic correction for superscript/subscript
+        labels.append(createMathLabel("U_3^2UY_3^2U_3Y^2f_1f^2ff", height:60))
+
+        // Error
+        labels.append(createMathLabel("\\notacommand", height:30))
+        
+        labels.append(createMathLabel("\\sqrt{1}", height:20))
+        labels.append(createMathLabel("\\sqrt[|]{1}", height:20))
+        labels.append(createMathLabel("{n \\choose k}", height:60))
+        labels.append(createMathLabel("{n \\choose k}", height:30))
+        self.labels[26].labelMode = .text
+        labels.append(createMathLabel("\\left({n \\atop k}\\right)", height:40))
+        labels.append(createMathLabel("\\left({n \\atop k}\\right)", height:30))
+        self.labels[28].labelMode = .text
+        labels.append(createMathLabel("\\underline{xyz}+\\overline{abc}", height:30))
+        labels.append(createMathLabel("\\underline{\\frac12}+\\overline{\\frac34}", height:50))
+        labels.append(createMathLabel("\\underline{x^\\overline{y}_\\overline{z}+5}", height:50))
+        
+        // spacing examples from the TeX book
+        labels.append(createMathLabel("\\int\\!\\!\\!\\int_D dx\\,dy", height:50))
+        // no spacing
+        labels.append(createMathLabel("\\int\\int_D dxdy", height:50))
+        labels.append(createMathLabel("y\\,dx-x\\,dy", height:30))
+        labels.append(createMathLabel("y dx - x dy", height:30))
+        
+        // large spaces
+        labels.append(createMathLabel("hello\\ from \\quad the \\qquad other\\ side", height:30))
+
+        // Accents
+        labels.append(createMathLabel("\\vec x \\; \\hat y \\; \\breve {x^2} \\; \\tilde x \\tilde x^2 x^2 ", height:30))
+        labels.append(createMathLabel("\\hat{xyz} \\; \\widehat{xyz}\\; \\vec{2ab}", height:30))
+        labels.append(createMathLabel("\\hat{\\frac12} \\; \\hat{\\sqrt 3}", height:50))
+
+        // large roots
+        labels.append( createMathLabel("\\colorbox{#f0f0e0}{\\sqrt{1+\\colorbox{#d0c0d0}{\\sqrt{1+\\colorbox{#a080c0}{\\sqrt{1+\\colorbox{#7050a0}{\\sqrt{1+\\colorbox{403060}{\\colorbox{#102000}{\\sqrt{1+\\cdots}}}}}}}}}}}", height:80))
+        
+        labels.append(createMathLabel("\\begin{bmatrix}" +
+                               "a & b\\\\ c & d \\\\ e & f \\\\ g &  h \\\\ i & j" +
+                               "\\end{bmatrix}", height:120))
+        labels.append(createMathLabel("x{\\scriptstyle y}z", height:30))
+        labels.append(createMathLabel("x \\mathrm x \\mathbf x \\mathcal X \\mathfrak x \\mathsf x \\bm x \\mathtt x \\mathit \\Lambda \\cal g", height:30))
+        labels.append(createMathLabel("\\mathrm{using\\ mathrm}", height:30))
+        labels.append(createMathLabel("\\text{using text}", height:30))
+        labels.append(createMathLabel("\\text{Mary has }\\$500 + \\$200.", height:30))
+        
+        labels.append(createMathLabel("\\colorbox{#888888}{\\begin{pmatrix}\\colorbox{#ff0000}{a} & \\colorbox{#00ff00}{b} \\\\ \\colorbox{#00aaff}{c} &" + "\\colorbox{#f0f0f0}{d}\\end{pmatrix}}", height:70))
         
         for i in 1..<labels.count {
             addLabel(withIndex: i, inArray: labels, toView: contentView)
---
+++ README.md
@@ -0,0 +1,20 @@
+# MathRenderDemo
+
+`MathRenderDemo` demonstrates the use of [MathRender](https://github.com/mgriebling/MathRender.git) within a traditional
+storyboard controlled by a viewcontroller.  Code is entirely written in Swift (except for the `MathRender` package).
+This demo is free of alien pods and the ensuing magical incantations that everyone hates.
+This same demo, although in Objective-C, is available from [iosMath](https://travis-ci.org/kostub/iosMath) (with alien pods).
+
+To use `MathRender` in your own code, just add the following package path to XCode: https://github.com/mgriebling/MathRender.git
+
+## Examples
+Here are screenshots of an iPad, iPhone, and Mac(catalyst) running the demo:
+
+![iPad Screenshot](images/iPad.png)
+
+![iPhone Screenshot](images/iPhone.png)
+
+![Quadratic Formula](images/iMac.png)
+
+
+
